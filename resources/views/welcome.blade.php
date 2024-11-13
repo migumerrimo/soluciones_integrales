7335171a3b17c0bb84a177bc50c64479cd3ced20
Miguel Angel <miguel1998er@gmail.com>
2024-11-13 11:58:21 -0600

    primer comit subida de proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOLUCIONES INTEGRALES</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <div class="background">
+        <div class="hexagon-container">
+        <!-- Hexágono 1 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/portones.jpg') }}" alt="Portones Eléctricos">
+                <p>INSTALACIÓN DE PORTONES ELÉCTRICOS</p>
+            </div>
+        </div>
+        <!-- Hexágono 2 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Seguridad.jpg') }}" alt="Seguridad">
+                <p>INSTALACIÓN DE SISTEMAS DE SEGURIDAD</p>
+            </div>
+        </div>
+        <!-- Hexágono 3 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Electricidad.jpg') }}"alt="Electricidad">
+                <p>ELECTRICIDAD EN GENERAL</p>
+            </div>
+        </div>
+        <!-- Hexágono 4 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Home.png') }}" alt="Logo">
+                <p>EASY HOME SOLUCIONES INTEGRALES</p>
+            </div>
+        </div>
+        <!-- Hexágono 5 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Huertos.jpg') }}" alt="Huertos">
+                <p>HUERTOS ECOLÓGICOS</p>
+            </div>
+        </div>
+        <!-- Hexágono 6 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Redes.jpg') }}" alt="Redes Industriales">
+                <p>REDES INDUSTRIALES Y RESIDENCIALES</p>
+            </div>
+        </div>
+        <!-- Hexágono 7 -->
+        <div class="hexagon" style="transform: rotate(30deg);">
+            <div class="hexagon-content" style="transform: rotate(330deg);">
+                <img src="{{ asset('images/Jardineria.jpg') }}" alt="Jardinería">
+                <p>JARDINERÍA & FUMIGACIÓN</p>
+            </div>
+        </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
