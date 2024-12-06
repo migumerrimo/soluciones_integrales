5df023248f8ce79b1401485aa1b4f148fa94a65e
Miguel Angel <miguel1998er@gmail.com>
2024-12-05 20:21:12 -0600

    botones_hexagonos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,54 +10,55 @@
     <div class="background">
         <div class="hexagon-container">
         <!-- Hexágono 1 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/portones.jpg') }}" alt="Portones Eléctricos">
                 <p>INSTALACIÓN DE PORTONES ELÉCTRICOS</p>
             </div>
-        </div>
+        </button>
+        
         <!-- Hexágono 2 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Seguridad.jpg') }}" alt="Seguridad">
                 <p>INSTALACIÓN DE SISTEMAS DE SEGURIDAD</p>
             </div>
-        </div>
+        </button>
         <!-- Hexágono 3 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Electricidad.jpg') }}"alt="Electricidad">
                 <p>ELECTRICIDAD EN GENERAL</p>
             </div>
-        </div>
+        </button>
         <!-- Hexágono 4 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Home.png') }}" alt="Logo">
                 <p>EASY HOME SOLUCIONES INTEGRALES</p>
             </div>
-        </div>
+        </button>
         <!-- Hexágono 5 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Huertos.jpg') }}" alt="Huertos">
                 <p>HUERTOS ECOLÓGICOS</p>
             </div>
-        </div>
+        </button>
         <!-- Hexágono 6 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Redes.jpg') }}" alt="Redes Industriales">
                 <p>REDES INDUSTRIALES Y RESIDENCIALES</p>
             </div>
-        </div>
+        </button>
         <!-- Hexágono 7 -->
-        <div class="hexagon" style="transform: rotate(30deg);">
+        <button class="hexagon" style="transform: rotate(30deg);">
             <div class="hexagon-content" style="transform: rotate(330deg);">
                 <img src="{{ asset('images/Jardineria.jpg') }}" alt="Jardinería">
                 <p>JARDINERÍA & FUMIGACIÓN</p>
             </div>
-        </div>
+        </button>
         </div>
 
     </div>
